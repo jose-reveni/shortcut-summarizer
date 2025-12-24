e538b91b425a286dd189b3b2461b62b863a4d1ea
José Enrique Narváez <jose@reveni.io>
2025-12-24 17:08:29 +0100

    GHpages?

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import electron from 'vite-plugin-electron'
-import renderer from 'vite-plugin-electron-renderer'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: '/shortcut-summarizer/',
     plugins: [
       react(),
     ],
