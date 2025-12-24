13e37cc1cc3a109e91db271a194a3da52e551e2f
José Enrique Narváez <jose@reveni.io>
2025-12-24 17:46:08 +0100

    Small fixes

--- services/shortcutService.ts
+++ services/shortcutService.ts
@@ -1,10 +1,9 @@
+
 import { ShortcutStory } from '../types';
 
 export class ShortcutService {
   private apiToken: string;
-  private baseUrl = (import.meta as any).env?.DEV
-    ? '/api/shortcut'
-    : 'https://api.thebugging.com/cors-proxy?url=https://api.app.shortcut.com/api/v3';
+  private baseUrl = (import.meta as any).env?.DEV ? '/api/shortcut' : 'https://api.app.shortcut.com/api/v3';
 
   constructor(apiToken: string) {
     this.apiToken = apiToken;
